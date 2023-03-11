70902bd327896cb645a56812ee8a520e2ec992ae
unknown <kk26092002@gmail.com>
2023-03-11 11:26:50 +0530

    hosting

--- db/db.sql
+++ db/db.sql
@@ -1,19 +1,11 @@
-CREATE TABLE restaurants( id bigserial NOT NULL , name VARCHAR(50) not null,
+CREATE TABLE restaurants( id bigserial PRIMARY KEY , name VARCHAR(50) not null,
  location VARCHAR(50) not null ,price_range int not null);
 
 insert into restaurants (id,name,location, price_range) values(123,'mcd','york',3);
 
 
-CREATE TABLE reviews (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
-    name VARCHAR(50) NOT NULL,
-    review TEXT NOT NULL,
-    rating INT NOT NULL check(
-        rating >= 1
-        and rating <= 5
-    )
-);
+CREATE TABLE reviews ( id BIGSERIAL NOT NULL PRIMARY KEY, restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),name VARCHAR(50) NOT NULL,review TEXT NOT NULL,rating INT NOT NULL check(rating >= 1 and rating <= 5));
+
 select *
 from restaurants
     left join(
--- db/index.js
+++ db/index.js
@@ -1,6 +1,19 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const pool = new Pool();
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+// const pool = new Pool();
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
+
+
+
+const { Pool } = require("pg");
+const PgPool = require("pg-pool");
+const pool = new PgPool({
+    connectionString: "postgres://kishore:hlN0p34lAUAFcxA9lRapoOcxktsg8UBQ@dpg-cg4pd2o2qv287cqdl4g0-a.singapore-postgres.render.com/yelp_7jc6",
+    ssl: {
+        rejectUnauthorized: false, // Set to true in production
+    },
+});
+
+module.exports = pool;
